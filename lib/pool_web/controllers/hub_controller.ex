e976e2ea41bfd9ecab57e23ad0ea06f75e308332
Josh Cobert <jdcobert@gmail.com>
2022-02-11 23:18:58 -0500

    integrate OpenWeather API for game weather

--- lib/pool_web/controllers/hub_controller.ex
+++ lib/pool_web/controllers/hub_controller.ex
@@ -2,6 +2,37 @@ defmodule PoolWeb.HubController do
   use PoolWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    weather = get_weather()
+    render(conn, "index.html", weather: weather)
+  end
+
+  # OpenWeather API calls
+  def geocode() do  # returns coordinates from city,[state],[country] input
+    endpoint = "http://api.openweathermap.org/geo/1.0/direct"
+    key = "90e450a4996778f8bb2bd339c6f51ef7"
+    city = "los angeles"
+    state = "ca"
+    country = ""
+    url = "#{endpoint}?q=#{city},#{state},#{country}&appid=#{key}"
+    res = HTTPoison.get!(url)
+    body = Poison.decode!(res.body, keys: :atoms)
+    data = List.first(body)
+    [data.lat, data.lon]
+  end
+
+  def get_weather() do  # returns temp and weather icon
+    endpoint = "api.openweathermap.org/data/2.5/weather"
+    key = "90e450a4996778f8bb2bd339c6f51ef7"
+    coords = geocode()
+    lat = Enum.at(coords, 0)
+    lon = Enum.at(coords, 1)
+    url = "#{endpoint}?lat=#{lat}&lon=#{lon}&appid=#{key}&units=imperial"
+    res = HTTPoison.get!(url)
+    body = Poison.decode!(res.body, keys: :atoms)
+    metrics = body.main
+    weather = List.first(body.weather)
+    icon = "http://openweathermap.org/img/wn/#{weather.icon}@2x.png"
+    weather_data = %{temp: round(metrics.temp), icon: icon}
+    weather_data
   end
 end
--- mix.exs
+++ mix.exs
@@ -50,7 +50,9 @@ defmodule Pool.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
+      {:httpoison, "~> 1.7"},
+      {:poison, "~> 5.0"}
     ]
   end
 
