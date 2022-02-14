bb3c22faba92dcbaad09c6b344aceabf1a5f3bff
Josh Cobert <jdcobert@gmail.com>
2022-02-13 23:12:59 -0500

    apply font awesome weather icons

--- lib/pool/weather/weather.ex
+++ lib/pool/weather/weather.ex
@@ -9,8 +9,7 @@ defmodule Pool.Weather do
     url = build_call(target, query)
     res = HTTPoison.get!(url)
     %{main: metrics, weather: [weather | _tail]} = Poison.decode!(res.body, keys: :atoms)
-    icon = "http://openweathermap.org/img/wn/#{weather.icon}@2x.png"
-    %{temp: round(metrics.temp), icon: icon}
+    %{temp: round(metrics.temp), icon: set_icon(weather.icon)}
   end
 
   # returns coordinates from city,[state],[country] input
@@ -31,5 +30,29 @@ defmodule Pool.Weather do
     endpoint = Keyword.fetch!(config, :endpoint_openweather)
     query = Map.put(query, "appid", key)
     endpoint <> target <> URI.encode_query(query)
+  end
+
+  # sets weather icon
+  def set_icon(code) do
+    case code do
+      "01d" -> "fa-solid fa-sun"
+      "01n" -> "fa-solid fa-moon-stars"
+      "02d" -> "fa-solid fa-sun-cloud"
+      "02n" -> "fa-solid fa-moon-cloud"
+      "03d" -> "fa-solid fa-cloud-sun"
+      "03n" -> "fa-solid fa-cloud-moon"
+      "04d" -> "fa-solid fa-clouds-sun"
+      "04n" -> "fa-solid fa-clouds-moon"
+      "09d" -> "fa-solid fa-cloud-drizzle"
+      "09n" -> "fa-solid fa-cloud-moon-rain"
+      "10d" -> "fa-solid fa-cloud-rain"
+      "10n" -> "fa-solid fa-cloud-moon-rain"
+      "11d" -> "fa-solid fa-cloud-bolt"
+      "11n" -> "fa-solid fa-cloud-bolt-moon"
+      "13d" -> "fa-solid fa-snowflakes"
+      "13n" -> "fa-solid fa-snowflakes"
+      "50d" -> "fa-solid fa-cloud-fog"
+      "50n" -> "fa-solid fa-cloud-fog"
+    end
   end
 end
