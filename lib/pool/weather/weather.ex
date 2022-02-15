e3bdd3351443ca075c67ec89affe022fd284600c
Josh Cobert <jdcobert@gmail.com>
2022-02-14 21:53:45 -0500

    more refactoring

--- lib/pool/weather/weather.ex
+++ lib/pool/weather/weather.ex
@@ -6,53 +6,25 @@ defmodule Pool.Weather do
     target = "/data/2.5/weather?"
     [lat, lon] = geocode("los angeles", "ca")
     query = %{"lat" => lat, "lon" => lon, "units" => "imperial"}
-    url = build_call(target, query)
-    res = HTTPoison.get!(url)
-    %{main: metrics, weather: [weather | _tail]} = Poison.decode!(res.body, keys: :atoms)
-    %{temp: round(metrics.temp), icon: set_icon(weather.icon)}
+    %{main: metrics, weather: [weather | _tail]} = make_call(target, query)
+    %{temp: round(metrics.temp), code: weather.icon}
   end
 
   # returns coordinates from city,[state],[country] input
   def geocode(city, state, country \\ "us") do
     target = "/geo/1.0/direct?"
     query = %{"q" => city <> "," <> state <> "," <> country}
-    url = build_call(target, query)
-    res = HTTPoison.get!(url)
-    body = Poison.decode!(res.body, keys: :atoms)
-    data = List.first(body)
-    [data.lat, data.lon]
+    [%{lat: lat, lon: lon} | _tail] = make_call(target, query)
+    [lat, lon]
   end
 
   # structures the API call url
-  def build_call(target, query) do
+  def make_call(target, query) do
     config = Application.get_env(:pool, __MODULE__)
     key = Keyword.fetch!(config, :secret_key_openweather)
     endpoint = Keyword.fetch!(config, :endpoint_openweather)
     query = Map.put(query, "appid", key)
-    endpoint <> target <> URI.encode_query(query)
-  end
-
-  # sets weather icon
-  def set_icon(code) do
-    case code do
-      "01d" -> "fa-solid fa-sun"
-      "01n" -> "fa-solid fa-moon-stars"
-      "02d" -> "fa-solid fa-sun-cloud"
-      "02n" -> "fa-solid fa-moon-cloud"
-      "03d" -> "fa-solid fa-cloud-sun"
-      "03n" -> "fa-solid fa-cloud-moon"
-      "04d" -> "fa-solid fa-clouds-sun"
-      "04n" -> "fa-solid fa-clouds-moon"
-      "09d" -> "fa-solid fa-cloud-drizzle"
-      "09n" -> "fa-solid fa-cloud-moon-rain"
-      "10d" -> "fa-solid fa-cloud-rain"
-      "10n" -> "fa-solid fa-cloud-moon-rain"
-      "11d" -> "fa-solid fa-cloud-bolt"
-      "11n" -> "fa-solid fa-cloud-bolt-moon"
-      "13d" -> "fa-solid fa-snowflakes"
-      "13n" -> "fa-solid fa-snowflakes"
-      "50d" -> "fa-solid fa-cloud-fog"
-      "50n" -> "fa-solid fa-cloud-fog"
-    end
+    res = HTTPoison.get!(endpoint <> target <> URI.encode_query(query))
+    Poison.decode!(res.body, keys: :atoms)
   end
 end
--- lib/pool_web/controllers/hub_controller.ex
+++ lib/pool_web/controllers/hub_controller.ex
@@ -2,9 +2,11 @@ defmodule PoolWeb.HubController do
   use PoolWeb, :controller
 
   alias Pool.Weather
+  alias PoolWeb.HubView
 
   def index(conn, _params) do
     weather_data = Weather.get_weather()
-    render(conn, "index.html", weather: weather_data)
+    weather_icon = HubView.return_icon()
+    render(conn, "index.html", weather: weather_data, icon: weather_icon)
   end
 end
