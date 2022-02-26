d9f083447d2e7361bb9b2bc0e8addaf146860a73
Josh Cobert <jdcobert@gmail.com>
2022-02-25 21:46:52 -0500

    replace string slice with at function

--- lib/pool_web/controllers/hub_controller.ex
+++ lib/pool_web/controllers/hub_controller.ex
@@ -2,7 +2,6 @@ defmodule PoolWeb.HubController do
   use PoolWeb, :controller
 
   alias Pool.Weather
-  alias PoolWeb.HubView
 
   def index(conn, _params) do
     weather_data = Weather.get_weather()
