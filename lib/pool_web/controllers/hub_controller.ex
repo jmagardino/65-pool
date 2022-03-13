41bc69e005da41365d455c5fb828c90bfca64e03
Josh Cobert <jdcobert@gmail.com>
2022-03-13 01:58:36 -0500

    add contests to dashboard

--- lib/pool_web/controllers/hub_controller.ex
+++ lib/pool_web/controllers/hub_controller.ex
@@ -4,11 +4,13 @@ defmodule PoolWeb.HubController do
   alias Pool.Weather
   alias Pool.SportsData
   alias Pool.Games
+  alias Pool.Contests
 
   def index(conn, _params) do
     weather_data = Weather.get_weather()
     games = Games.list_games()
+    contests = Contests.list_contests()
     date = DateTime.utc_now()
-    render(conn, "index.html", games: games, date: date, weather: weather_data, team_data: SportsData)
+    render(conn, "index.html", games: games, contests: contests, date: date, weather: weather_data, team_data: SportsData)
   end
 end
